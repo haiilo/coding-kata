5f8adb6c7938bd2aea11bede6b3fb1fa69621784
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2023-01-31 11:32:50 +0100

    setup catalyst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { CatI18nRegistry, CatIconRegistry } from '@haiilo/catalyst';
+import {
+  CAT_I18N_REGISTRY_TOKEN,
+  CAT_ICON_REGISTRY_TOKEN,
+} from '@haiilo/catalyst-angular';
+import { ci } from '@haiilo/catalyst-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    @Inject(CAT_ICON_REGISTRY_TOKEN)
+    private readonly iconRegistry: CatIconRegistry,
+    @Inject(CAT_I18N_REGISTRY_TOKEN)
+    private readonly i18nRegistry: CatI18nRegistry
+  ) {
+    this.addCatalystIcons();
+    this.addCatalystTranslations();
+  }
+
+  private addCatalystIcons(): void {
+    this.iconRegistry.addIcons(ci);
+  }
+
+  private addCatalystTranslations(): void {
+    this.i18nRegistry.set({
+      'dialog.close': 'Close',
+      'input.clear': 'Clear',
+      'input.optional': 'Optional',
+      'input.required': 'Required',
+      'notification.dismiss': 'Dismiss',
+      'pagination.prev': 'Previous',
+      'pagination.page': 'Go to page {{page}}',
+      'pagination.next': 'Next',
+      'select.close': 'Close',
+      'select.deselect': 'Deselect',
+      'select.empty': 'No items',
+      'select.open': 'Open',
+    });
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CatalystModule } from '@haiilo/catalyst-angular';
 import { AppComponent } from './app.component';
 import { StageComponent } from './stage/stage.component';
 
 @NgModule({
+  imports: [BrowserModule, CatalystModule.forRoot()],
   declarations: [AppComponent, StageComponent],
-  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
